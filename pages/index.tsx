8a355abc4213f4809a2bb96343b4448054f195a5
scooo <scooomaker@foxmail.com>
2022-08-26 11:50:56 +0800

    add nav func

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Navbar />
       
     </>
   )
