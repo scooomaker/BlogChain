23e10eda5da95be8d878578ea2825dfe5a465145
scooo <scooomaker@foxmail.com>
2022-08-28 08:45:34 +0800

    init posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,18 @@ import LittleNav from '../components/LittleNav'
 import Navbar from '../components/Navbar'
 import MainText from '../components/MainText'
 import BgImg from '../components/BgImg'
-
+import Head from 'next/head'
 
 
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Blog</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
       <div className='bg-slate-200 h-[10000px]'>
         <Navbar />
         <BgImg />
