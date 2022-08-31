dbeb2d75883b6960ffb62692468d872b75c348b5
scooo <scooomaker@foxmail.com>
2022-08-31 15:44:28 +0800

    fix bugs

--- pages/blog/page.jsx
+++ pages/blog/page.jsx
@@ -119,11 +119,11 @@ export default function Blog({ posts }) {
 
             <div key={slug} className="h-52 pt-5 pl-5 pr-2 pb-5  mt-3 bg-stone-50 drop-shadow-xl rounded-lg mx-2 ">
               <Link href={`/blog/${slug}`}>
-                <div className='h-52 pl-4 pr-2 pb-5 cursor-pointer'>
+                <div className='h-52 pl-2 pr-2 pb-5 cursor-pointer'>
                   <h1 className='text-3xl truncate'>{frontmatter.title}</h1>
                   <div className='font-serif truncate'>{frontmatter.date}</div>
                   <hr />
-                  <div className='h-[105px] font-light px-3 pt-2 indent-8 discript-text'>
+                  <div className='h-[105px] font-light px-3 pt-2 indent-8 discript-text-mobile'>
                     {frontmatter.descript}
                   </div>
 
