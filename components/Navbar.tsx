1d4b66f14393c9bbcff33297d0492df4e6e73a28
scooo <scooomaker@foxmail.com>
2022-08-31 10:49:16 +0800

    modify Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,146 +42,19 @@ export default function Navbar(props: { name: string }) {
       {({ open }) => (
         <>
           <div className="mx-auto px-2">
-            <div className="relative flex items-center justify-between mobile:h-[80px] pad:h-[100px] pc:h-[120px] 4k:h-[150px]">
+            <div className="relative flex items-center justify-end mobile:h-[80px] pad:h-[100px] pc:h-[120px] 4k:h-[150px]">
 
-              <div className="absolute inset-y-0 left-0 flex items-center">
-
-                {/* Mobile menu button*/}
-                <div className='invisible md:visible'>
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-sky-200
+              <div className=''>
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 
                  ">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-9 w-9" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-9 w-9" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
               </div>
-
-
-              <div className="flex-1 flex items-center justify-start m-12 md:hidden">
-
-                {/* image here */}
-                {/* <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src={logo}
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src={logo}
-                    alt="Workflow"
-                  />
-                </div> */}
-                {/* image here */}
-
-
-                {/* add the navigation item */}
-                <div className="ml-5">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <>
-                        {/* here you should add icons */}
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current ? 'bg-sky-200 text-black' : 'text-slate-500 hover:bg-sky-50 hover:text-inherit',
-                            'px-3 py-2 rounded-md text-[1.2vw]  pad:text-[18px] 4k:text-[25px] font-nav font-[20] uppercase'
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
-                      </>
-
-                    ))}
-                  </div>
-                </div>
-                {/* add the navigation item */}
-
-
-              </div>
-
-
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 ">
-
-                {/* View notifications button start */}
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-                {/* View notifications button end  */}
-
-
-                {/* Profile start */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src={profile_img}
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-                {/* Profile end */}
-
-
-              </div>
-
-
 
             </div>
           </div>
