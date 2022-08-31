fa17bc737e2a0f8e5b1d4be0c18ec34b1da96e8c
scooo <scooomaker@foxmail.com>
2022-08-31 17:23:52 +0800

    modify the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,42 @@
 import React from 'react'
-
+import { VscGithubInverted } from "react-icons/vsc";
+import { RiBilibiliFill } from "react-icons/ri";
+import { AiOutlineMail } from "react-icons/ai";
+import { IconContext } from "react-icons";
 function Footer() {
   return (
     <>
-      <footer className='bg-black mt-12 text-white'>
-        <div className='grid mobile:grid-cols-1 pad:grid-cols-2 
-        max-w-[980px] mx-auto  grid-cols-3 gap-3 justify-items-center'>
+      {/* grid mobile:grid-cols-1 pad:grid-cols-2  grid-cols-3 gap-3 */}
+      <footer className='bg-black mt-12 text-white py-[40px]'>
+        <div className='max-w-[1000px] mx-10 px-auto justify-between flex sm:block sm:text-center'>
 
-          <div>
-            <span className='text-lg font-normal'>Blog</span>
-          </div>
+          <p>© 2022. All rights reserved by scooo.</p>
+          <ul className='flex space-x-5 sm:justify-center sm:pt-3'>
+            <li>
 
-          <div>
-            <span>Project</span>
-          </div>
+              <a href="https://github.com/scooomaker">
+                <IconContext.Provider value={{size: '1.8em'}}>
+                  <VscGithubInverted/>
+                </IconContext.Provider>
+              </a>
 
-          <div>
-            <span>Me</span>
-          </div>
+            </li>
+
+            <li>
+              <a href="https://space.bilibili.com/353271021">
+                <IconContext.Provider value={{size: '1.8em'}}>
+                  <RiBilibiliFill/>
+                </IconContext.Provider>
+              </a>
+            </li>
+            <li>
+              <a href="mail:scooomaker@foxmail.com">
+                <IconContext.Provider value={{size: '1.8em'}}>
+                  <AiOutlineMail/>
+                </IconContext.Provider>
+              </a>
+            </li>
+          </ul>
 
         </div>
       </footer>
