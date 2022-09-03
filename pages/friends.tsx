ac55ac86128ab65a9532c10d1e4be7ed6c0b60d5
scooo <scooomaker@foxmail.com>
2022-09-03 23:56:32 +0800

    add friends

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,18 +1,42 @@
+import Image from 'next/image'
 import React from 'react'
 import Navbar from '../components/Navbar'
 
 
 const profileImg = '/images/sea1.png'
 
+const myfriends = [
+  {
+    name: '瞎哔哔的py',
+    href: 'https://whisper.pyliubaolin.top/',
+    img: 'https://oss.yzbh.tj.cn/halo/avatar.png',
+    description: '一秒接过制服，一生志在蓝天！'
+  },
+  {
+    name: 'ocero',
+    href: 'https://ocero-blog.vercel.app/',
+    img: 'https://pic.rmb.bdstatic.com/bjh/85ee05046dc5b572bd577f471829b5d1.jpeg',
+    description: '一个单调而有趣的博客'
+  },
+  {
+    name: 'GUOREN',
+    href: 'https://guorenjun.xyz/',
+    img: 'https://pic.rmb.bdstatic.com/bjh/59a0860cceef75aaa60730346daf4d00.jpeg',
+    description: 'None'
+  },
+]
+
+
+
 
 function friends() {
   return (
     <>
       <Navbar name='friends' />
-      <main className='h-screen'>
-        <div className="max-w-[1200px] mx-auto apple text-[48px] py-[5vh] px-[8vw] font-bold">
-          Me
-          <hr className='mt-5' />
+      <main>
+        <div className="font-bold my-32 px-5">
+          <p className='text-[48px] max-w-[800px] mx-auto px-3'>Me</p>
+          <hr className='mt-16 max-w-[800px] mx-auto' />
 
 
           {/* <div className='h-auto flex my-5 bg-blue-50'>
@@ -25,6 +49,31 @@ function friends() {
               <p></p>
             </div>
           </div> */}
+
+          <div className='h-[30vh]'></div>
+
+          <p className='text-[48px] max-w-[800px] mx-auto px-3'>My Friends</p>
+          <hr className='my-16 max-w-[800px] mx-auto' />    
+          
+          <div className='max-w-[1000px] mx-auto grid grid-cols-3 lg:grid-cols-2 sm:block'>
+            {myfriends.map((item) => (
+              <div className='rounded-2xl overflow-hidden mx-2 my-5 tracking-[0.012em]'>
+                <div className='card-img'>
+                  <img src={item.img} alt={item.name} className='w-full h-full object-cover'/>
+                </div>
+                <div className='p-4'>
+                  <p className='text-2xl font-bold my-6'>{item.name}</p>
+                  <p className='text-lg font-normal my-6'>{item.description}</p>
+                </div>
+                <div className='ml-auto flex justify-end'>
+                  <a href={item.href} className='bg-blue-600 text-white px-3  rounded-[32px] text-[19px] font-light hover:bg-blue-800'>Visit</a>
+                </div>
+
+              </div>
+            ))
+
+            }
+          </div>
 
           
         </div>
