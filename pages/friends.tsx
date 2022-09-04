19fa5e595ec95dfe275c8a003e0342249d65824e
scooo <scooomaker@foxmail.com>
2022-09-04 10:20:18 +0800

    modify friends.tsx and update body css

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Disclosure } from '@headlessui/react'
+import { ChevronUpIcon } from '@heroicons/react/solid'
 
 
-const profile = [
-  {
-    name: 'FengXiao',
-    img: '/images/sea1.png',
-    description: 'I am a student from China',
-  }
-]
+
+const MyProfile = {
+  name: 'FengXiao',
+  img: '/images/sea1.png',
+  description: 'I am a student from China'
+};
+
 
 const myfriends = [
   {
@@ -39,49 +41,22 @@ const myfriends = [
 
 
 function friends() {
+
+
+
   return (
     <>
       <Navbar name='friends' />
       <main>
         <div className="font-bold my-32 px-5">
-          <p className='text-[48px] max-w-[800px] mx-auto px-3'>Me</p>
-          <hr className='mt-16 max-w-[800px] mx-auto' />
-
-
-          {/* <div className='h-auto flex my-5 bg-blue-50'>
-            <div className='w-[128px]'>
-              <img src={profileImg} alt="" className="rounded-lg"/>
-            </div>
-            
-            <div className='ml-9'>
-              <p className='text-[30px]'>FengXiao</p>
-              <p></p>
-            </div>
-          </div> */}
-
-          {/* <div className='rounded-2xl overflow-hidden mx-2 my-5 tracking-[0.012em]'>
-            <div className='card-img'>
-              <img src={} alt={} className='w-full h-full object-cover' />
-            </div>
-            <div className='p-4'>
-              <p className='text-2xl font-bold my-6'>{}</p>
-              <p className='text-lg font-normal my-6'>{}</p>
-            </div>
-            <div className='ml-auto flex justify-end'>
-              <a href={} className='bg-blue-600 text-white px-3  rounded-[32px] text-[19px] font-light hover:bg-blue-800'>Visit</a>
-            </div>
-
-          </div> */}
-
-
-          <div className='h-[30vh]'></div>
+          
 
           <p className='text-[48px] max-w-[800px] mx-auto px-3'>My Friends</p>
           <hr className='my-16 max-w-[800px] mx-auto' />
 
           <div className='max-w-[1000px] mx-auto grid grid-cols-3 lg:grid-cols-2 sm:block'>
             {myfriends.map((item) => (
-              <div className='rounded-2xl overflow-hidden mx-2 my-5 tracking-[0.012em]' key={item.id}>
+              <div className='bg-[#fff] dark:bg-[#181818] rounded-2xl overflow-hidden mx-2 my-5 tracking-[0.012em]' key={item.id}>
                 <div className='card-img'>
                   <img src={item.img} alt={item.name} className='w-full h-full object-cover' />
                 </div>
@@ -97,6 +72,49 @@ function friends() {
             ))
 
             }
+          </div>
+
+
+
+          <p className='text-[48px] max-w-[800px] mx-auto px-3 mt-32'>Me</p>
+          <hr className='my-16 max-w-[800px] mx-auto' />
+
+          <div className='max-w-[700px] mx-auto '>
+            <div className='rounded-3xl overflow-hidden mx-2 my-5 tracking-[0.012em] grid grid-flow-row-dense grid-cols-2 gap-3 sm:grid-cols-1 bg-[#fff] dark:bg-[#181818]'>
+
+              <div className='card-img'>
+                <img src={MyProfile.img} alt={MyProfile.name} className='w-full h-full object-cover' />
+              </div>
+
+              <div>
+                <div className='p-4'>
+                  <p className='text-4xl font-bold my-6'>{MyProfile.name}</p>
+                  <p className='text-xl font-normal my-6'>{MyProfile.description}</p>
+
+                  <Disclosure as="div" className="mt-2">
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button className="flex w-full justify-between rounded-lg bg-blue-100 px-4 py-2 text-left text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                          <span>Contact Me</span>
+                          <ChevronUpIcon
+                            className={`${open ? 'rotate-180 transform' : ''
+                              } h-5 w-5 text-blue-500`}
+                          />
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                          My WeChat is a_zero_40
+                          <br />
+                          My email is scooomaker@foxmail.com
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+
+                </div>
+              </div>
+
+
+            </div>
           </div>
 
 
